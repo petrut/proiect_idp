15435f2584c51d4e839a9a8a2dc267b30e1de65d
Petrut <terispetrut@gmail.com>
2014-04-15 20:04:44 +0300

    test logare in main (configurare log4j.properties)

--- Proiect_idp/src/common/Main.java
+++ Proiect_idp/src/common/Main.java
@@ -1,18 +1,23 @@
 package common;
 
 import gui.GuiAPI;
-
+import test.MockupMediator;
 import java.awt.EventQueue;
 
-import test.MockupMediator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 
 // clasa temporara pentru testarea aplicatiei (partea grafica)
 
 public class Main {
 	
-	static String current_user = "me";
+	static String current_user = "me";	
+	static Logger logger = Logger.getLogger(Main.class);
 
 	public static void main(String []args) throws InterruptedException{
+		
+		String log_path;
 		
 		if(args.length > 0){
 			if(!args[0].equals("${args}"))
@@ -20,6 +25,16 @@ public class Main {
 		}
 		
 		System.out.println("> current user = " + current_user);
+		
+		log_path = "Users_info/" + current_user + "/log4j.properties";		
+		PropertyConfigurator.configure(log_path);
+				
+		logger.debug("Test debug message");
+		logger.info("Test info message");
+		logger.warn("Test warn message");
+		logger.error("Test error message");
+		logger.fatal("Test fatal message");
+
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
